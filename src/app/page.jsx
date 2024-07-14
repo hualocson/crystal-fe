45bcc8b744cd6213b3c72db0bad15c09859552e6
locson <hualocson@gmail.com>
2024-07-14 12:57:33 +0700

    ADD UI for Main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,20 @@
+import BorderBox from "@/components/border-box";
 import HelloWorldPanel from "@/components/hello-world-panel";
+import { ImageSlider } from "@/components/project-showcases/image-slider";
+
+import images from "@/resources";
 
 const MainPage = () => {
-  return <HelloWorldPanel />;
+  return (
+    <div className="flex flex-col gap-10 z-50 relative">
+      <HelloWorldPanel />
+      <BorderBox>
+        <div className="p-4">
+          <ImageSlider images={images} />
+        </div>
+      </BorderBox>
+    </div>
+  );
 };
 
 export default MainPage;
