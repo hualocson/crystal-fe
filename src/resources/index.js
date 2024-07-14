29720912881ca3c90ecfff3c18c04a82a5c4f245
locson <hualocson@gmail.com>
2024-07-14 12:56:56 +0700

    ADD image resources

---
+++ src/resources/index.js
@@ -0,0 +1,8 @@
+import image1 from "./merge_01.jpg";
+import image2 from "./merge_02.jpg";
+import image3 from "./merge_03.jpg";
+import image4 from "./merge_04.jpg";
+
+const images = [image1, image2, image3, image4];
+
+export default images;
