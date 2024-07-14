70ba7412d116c2e9f7bb4a930e10d91c9cffb7ab
locson <hualocson@gmail.com>
2024-07-14 12:59:03 +0700

    ADD text for pannel

--- src/components/hello-world-panel.jsx
+++ src/components/hello-world-panel.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 const HelloWorldPanel = () => {
+  const router = useRouter();
   return (
-    <div className="flex flex-col gap-6 border border-white/15 bg-slate-500/15 p-6 rounded-lg max-w-screen-md">
-      <span className="text-4xl font-bold">Hello World</span>
-      <span className="text-xl">
-        Introducing a new way to interact with astica cognitive intelligence.
+    <div className="flex flex-col gap-6 border border-white/15 bg-slate-500/15 p-6 rounded-md max-w-screen-lg">
+      <span className="text-4xl font-bold">
+        AmoSeg: Công cụ nhận diện tinh thể bị che khuất
       </span>
       <span>
-        Improved capability, documentation, and more functionality now at your
-        disposal.
+        Ứng dụng nhận diện tinh thể của chúng tôi sử dụng công nghệ học máy tiên
+        tiến để xác định và đo kích thước tinh thể, kể cả những phần bị che
+        khuất
+      </span>
+      <span>
+        <Button onClick={() => router.push("/crystal-ai")}>Sử dụng thử</Button>
       </span>
     </div>
   );
