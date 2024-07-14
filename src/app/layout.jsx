aa7aba65b1f87855bad30aba9122b8f4fa2ead1e
locson <hualocson@gmail.com>
2024-07-14 12:57:17 +0700

    UPDATE: change theme

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={cn(chakraPetch.className, "")}>
         <MainLayout>{children}</MainLayout>
       </body>
